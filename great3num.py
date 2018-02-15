8f463fddae09968826b9321b8a23c93f348d8054
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-02-15 14:17:22 +0530

    great3num
    to find the greatest among 3 nums

---
+++ great3num.py
@@ -0,0 +1,25 @@
+a=int(input("enter the value of a:"))
+
+b=int(input("enter the value of b:"))
+
+c=int(input("enter the value of c:"))
+
+if(a>b):
+
+  if(a>c):
+
+    print("a is greatest num")
+
+  else:
+
+    print("c is greatest num")
+
+else:
+
+  if(b>c):
+
+    print("b is greatest num")
+
+  else:
+
+    print("c is greatest num")
