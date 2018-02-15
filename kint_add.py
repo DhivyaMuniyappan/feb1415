666dd8011dd5f859e437328909da76803a61d55b
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-02-15 16:01:01 +0530

    kint_add

---
+++ kint_add.py
@@ -0,0 +1,17 @@
+n=int(input("enter the value of n :"))
+
+k=int(input("enter the value of k :"))
+
+myarray=[2,3,4,5,6,7,8,9]
+
+i=0
+
+sum=0
+
+while(i<k):
+
+  sum+=myarray[i]
+
+  i=i+1
+
+print(sum)
